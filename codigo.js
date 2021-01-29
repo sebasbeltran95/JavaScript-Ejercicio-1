c5bb24615802a5b5d5af483ce03959ee979ced9f
sebastian beltran zapata <sebastian199502@hotmail.com>
2021-01-29 15:20:42 -0500

    Ejercicio 1 JavaScript

---
+++ codigo.js
@@ -0,0 +1,54 @@
+let dineroCofla = prompt("¿Cuanto dinero tienes Cofla?");
+let dineroRoberto = prompt("¿Cuanto dinero tienes Roberto?");
+let dineroPedro = prompt("¿Cuanto dinero tienes Pedro?");
+
+if (dineroCofla >= 0.6 && dineroCofla < 1){
+    alert(" cofla, comprate el helado de agua y su vuelta es: " +  (dineroCofla - 0.6));
+} else if(dineroCofla >= 1 && dineroCofla < 1.6){
+    alert("cofla, comprate el helado de crema y su vuelta es: " +  (dineroCofla - 1));
+} else if(dineroCofla >= 1.6 && dineroCofla < 1.7){
+    alert("cofla, comprate el helado de heladixy su vuelta es: " +  (dineroCofla - 1.6));
+} else if(dineroCofla >= 1.7 && dineroCofla < 1.8){
+    alert("cofla, comprate el helado de heladovich y su vuelta es: " +  (dineroCofla - 1.7));
+}else if (dineroCofla >= 1.8 && dineroCofla < 2.9){
+    alert("cofla, comprate el helado de helardo y su vuelta es: " +  (dineroCofla - 1.8));
+} else if(dineroCofla >= 2.9){
+    alert("cofla, comprate el helado con confites o el pote de 1/4 kg y su vuelta es: " +  (dineroCofla - 2.9));
+}else {
+    alert("lo siento cofla, pobre de mierda, no te alcanza para ningun helado XD");
+}
+
+if (dineroRoberto >= 0.6 && dineroRoberto < 1){
+    alert(" roberto, comprate el helado de agua y su vuelta es: " +  (dineroRoberto - 0.6));
+} else if(dineroRoberto >= 1 && dineroRoberto < 1.6){
+    alert("roberto, comprate el helado de crema y su vuelta es: " +  (dineroRoberto - 1));
+} else if(dineroRoberto >= 1.6 && dineroRoberto < 1.7){
+    alert("roberto, comprate el helado de heladix y su vuelta es: " +  (dineroRoberto - 1.6));
+} else if(dineroRoberto >= 1.7 && dineroRoberto < 1.8){
+    alert("roberto, comprate el helado de heladovich y su vuelta es: " +  (dineroRoberto - 1.7));
+}else if (dineroRoberto >= 1.8 && dineroRoberto < 2.9){
+    alert("roberto, comprate el helado de helardo y su vuelta es: " +  (dineroRoberto - 1.8));
+} else if(dineroRoberto >= 2.9){
+    alert("roberto, comprate el helado con confites o el pote de 1/4 kg y su vuelta es: " +  (dineroRoberto - 2.9));
+}else {
+    alert("lo siento roberto, pobre de mierda, no te alcanza para ningun helado XD");
+}
+
+if (dineroPedro >= 0.6 && dineroPedro < 1){
+    alert("pedro, comprate el helado de agua y su vuelta es: " +  (dineroPedro - 0.6));
+} else if(dineroPedro >= 1 && dineroPedro < 1.6){
+    alert("pedro, comprate el helado de crema y su vuelta es: " +  (dineroPedro - 1));
+} else if(dineroPedro >= 1.6 && dineroPedro < 1.7){
+    alert("pedro, comprate el helado de heladix y su vuelta es: " +  (dineroPedro - 1.6));
+} else if(dineroPedro >= 1.7 && dineroPedro < 1.8){
+    alert("pedro, comprate el helado de heladovich y su vuelta es: " +  (dineroPedro - 1.7));
+}else if (dineroPedro >= 1.8 && dineroPedro < 2.9){
+    alert("pedro, comprate el helado de helardo y su vuelta es: " +  (dineroPedro - 1.8));
+} else if(dineroPedro >= 2.9){
+    alert("pedro, comprate el helado con confites o el pote de 1/4 kg y su vuelta es: " +  (dineroPedro - 2.9));
+}else {
+    alert("lo siento pedro, pobre de mierda, no te alcanza para ningun helado XD");
+}
+
+
+
